fda0cf6a58a1abbe0c7c2ac1b03656a893629ab6
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-29 12:06:13 +0100

    update caller-1 to use `rushDeliveryDate` instead of `deliveryDate`

--- src/caller-1.js
+++ src/caller-1.js
@@ -1,9 +1,9 @@
-import { deliveryDate } from './get-delivery-date/index.js';
+import { rushDeliveryDate } from './get-delivery-date/index.js';
 import { ManagedDate } from './managed-date/index.js';
 
 const anOrder = { deliveryState: 'MA', placedOn: new ManagedDate() };
 
 const aShipment = {};
-aShipment.deliveryDate = deliveryDate(anOrder, true);
+aShipment.deliveryDate = rushDeliveryDate(anOrder);
 
 console.log(`Order will be delivered on: ${aShipment.deliveryDate.toString()}`);
